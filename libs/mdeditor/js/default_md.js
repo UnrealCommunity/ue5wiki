eaf8a1490c131694f52a2d752a955f5a0c55a0c6
imzlp <imzlp@foxmail.com>
2024-10-24 12:13:18 +0000

    Site updated: 2024-10-24 12:13:15

---
+++ libs/mdeditor/js/default_md.js
@@ -0,0 +1,30 @@
+var default_md = `---
+title: #标题
+date: 2021-06-29 22:32:01 #创建时间
+toc: true #开启目录
+categories:
+  - #分类1
+tag: #标签可以指定多个（每行一个）
+  - #标签1
+comments: true #开启评论
+ue_version: UE4.25 #文章使用的引擎版本
+author: #作者信息，会显示在贡献者列表
+  name: #名字
+  site_addr: #网站链接
+  avatar_image: #头像图片链接，如果为空则默认使用github头像
+  github_id: #github账户ID
+  zhihu_addr: #知乎链接
+  email: #邮件地址
+  wechat_image: #微信二维码图片地址
+postQR:
+  enable: false #开启文章末尾二维码
+  image: #二维码链接
+  info: #显示文字
+copyright: #文章末尾的版权信息
+  enable: true
+  history: true #是否可查看文章编辑历史、RAW文本
+  license: CC BY-NC-SA 4.0 #文章的授权协议
+  backupAddr: #文章的备份链接
+---
+
+`
